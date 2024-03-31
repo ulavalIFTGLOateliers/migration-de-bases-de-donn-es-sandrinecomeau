98bda0deadbeaf5727dc8af3e8168e5623e9f8cb
sandrinecomeau <84459648+sandrinecomeau@users.noreply.github.com>
2024-03-31 01:33:05 -0400

    remise

--- db_scripts/migrate_1.sql
+++ db_scripts/migrate_1.sql
@@ -1,1 +1,24 @@
 ALTER TABLE singer RENAME TO musician;
+ALTER TABLE musician ADD COLUMN role VARCHAR(50);
+ALTER TABLE musician ADD COLUMN bandName VARCHAR(50);
+ALTER TABLE musician CHANGE singerName musicianName VARCHAR(50);
+
+CREATE TABLE IF NOT EXISTS band (
+    bandName VARCHAR(50),
+    creation YEAR,
+    genre VARCHAR(50),
+    PRIMARY KEY (bandName)
+);
+
+INSERT INTO band (bandName, creation, genre) VALUES
+('Crazy Duo', 2015, 'rock'),
+('Luna', 2009, 'classical'),
+('Mysterio', 2019, 'pop');
+
+ALTER TABLE album DROP FOREIGN KEY FK_A_singerName;
+ALTER TABLE album ADD CONSTRAINT FK_A_singerName FOREIGN KEY (singerName) REFERENCES musician (musicianName);
+
+UPDATE musician SET role='vocals', bandName='Crazy Duo' WHERE musicianName='Alina';
+UPDATE musician SET role='guitar', bandName='Mysterio' WHERE musicianName='Mysterio';
+UPDATE musician SET role='percussion', bandName='Crazy Duo' WHERE musicianName='Rainbow';
+UPDATE musician SET role='piano', bandName='Luna' WHERE musicianName='Luna';
